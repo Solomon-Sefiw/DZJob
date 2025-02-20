3d1cc877f01568cdeafb7aed8ff7323a8959ca54
Solomon S. <solomon.sefiw@berhanbanksc.com>
2025-02-20 01:50:52 -0800

    Update CreateJobApplicationCommand.cs

--- DZJobs.Application/Features/JobApplication/Commands/CreateJobApplication/CreateJobApplicationCommand.cs
+++ DZJobs.Application/Features/JobApplication/Commands/CreateJobApplication/CreateJobApplicationCommand.cs
@@ -4,7 +4,7 @@ using HCMS.Services.DataService; // Replace with your actual DbContext namespace
 
 namespace HCMS.Application.JobApplications.Commands
 {
-    internal class CreateJobApplicationCommand
+    public class CreateJobApplicationCommand : IRequest<int>
     {
         public int JobId { get; set; }
         public string FreelancerId { get; set; }
