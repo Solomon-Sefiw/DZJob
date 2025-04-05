9bd863f07f1b6c6c0ce39286f4a3dca5917d9400
Solomon S. <solomon.sefiw@berhanbanksc.com>
2025-04-05 10:52:55 +0300

    config

--- dz-jobs/api-config.ts
+++ dz-jobs/api-config.ts
@@ -1,6 +1,6 @@
 const config: any = {
-    // schemaFile: "https://dzjobs-api.somee.com/swagger/v1/swagger.json", // Swagger URL remote
-    schemaFile: "https://localhost:7078/swagger/v1/swagger.json", // Swagger URL local
+     schemaFile: "https://dzjobs-api.somee.com/swagger/v1/swagger.json", // Swagger URL remote
+    //schemaFile: "https://localhost:7078/swagger/v1/swagger.json", // Swagger URL local
     apiFile: "./src/app/services/emptySplitApi.ts",                  // Base API file
     apiImport: "emptySplitApi",                                  // Import from base API
     outputFile: "./src/app/services/DZJobsApi.ts",                     // Output API file
